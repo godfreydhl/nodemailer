307b4851067f32070309db8405963e026441f71e
godfreydhl <32124323+godfreydhl@users.noreply.github.com>
2023-09-01 16:05:04 +0200

    email sent

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const env = require("dotenv")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const app = express();
+
+env.config();
+const transporter = nodemailer.createTransport({
+    service : 'gmail',
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+app.get('/', (req, res)=>{
+    transporter.sendMail({
+        from: 'John Doe <'+process.env.EMAIL + '>',
+        to: 'godfreydhl@gmail.com', 
+        subject: 'Testing nidemailer',
+        html: `<h1>Heading</h1>
+        <p> This is a nodemailer email. Well done G</p>`
+    }, (err, info)=>{
+        if(err) throw err;
+        res.json({status:info.response})
+    })
+    
+})
+
+app.listen(7000, ()=>{
+    console.log('Running on 7000')
+})
